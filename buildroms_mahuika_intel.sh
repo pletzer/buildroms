6c84ffd36fd6e6e5c4474040554a6dba4d105500
Alex Pletzer <alexander@gokliya.net>
2021-11-16 01:10:01 +0000

    work in progress

--- buildroms_mahuika_intel.sh
+++ buildroms_mahuika_intel.sh
@@ -1,10 +1,19 @@
 ml intel
 ml netCDF
 
+
+
 export NETCDF_INCDIR=$EBROOTNETCDF/include
 export NETCDF_LIBDIR=$EBROOTNETCDF/lib64
 export HDF5_LIBDIR=$EBROOTHDF5/lib
 
+export USE_DEBUG=OFF
+export USE_MPI=ON
+export USE_MPIF90=ON
+export USE_OPENMP=ON
+export USE_LARGE=ON
+export USE_NETCDF4=ON
 
-
+export FORT=ifort
+export BINDIR=bin_mahuika_intel
 
